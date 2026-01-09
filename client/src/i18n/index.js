ca6b3359451c18fe5995c9b2cd94470a3259b408
Cxvarisorka <lcxvaradze2006@gmail.com>
2026-01-10 03:52:32 +0400

    Add internationalization with support for EN, ES, RU, and KA languages

---
+++ client/src/i18n/index.js
@@ -0,0 +1,30 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './locales/en.json';
+import es from './locales/es.json';
+import ru from './locales/ru.json';
+import ka from './locales/ka.json';
+
+const resources = {
+  en: { translation: en },
+  es: { translation: es },
+  ru: { translation: ru },
+  ka: { translation: ka }
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en',
+    fallbackLng: 'en',
+    supportedLngs: ['en', 'es', 'ru', 'ka'],
+    interpolation: {
+      escapeValue: false
+    },
+    react: {
+      useSuspense: false
+    }
+  });
+
+export default i18n;
