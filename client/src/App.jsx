50777e7e3fb61a1f126f8cbd48c659863a7d200f
Cxvarisorka <lcxvaradze2006@gmail.com>
2026-01-18 04:48:02 +0400

    Update app configuration, styles, and translations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,24 @@ import { Home } from './pages/Home';
 import { Transfers } from './pages/Transfers';
 import { CarRentals } from './pages/CarRentals';
 import { CarRentalDetail } from './pages/CarRentalDetail';
+import { Tours } from './pages/Tours';
+import { TourDetail } from './pages/TourDetail';
 import { Contact } from './pages/Contact';
 import { SignIn } from './pages/SignIn';
 import { SignUp } from './pages/SignUp';
 import { UserProfile } from './pages/UserProfile';
 import { AdminProvider } from './context/AdminContext';
 import { UserProvider } from './context/UserContext';
+import { SocketProvider } from './context/SocketContext';
 import { AdminLayout } from './components/admin/AdminLayout';
 import { AdminDashboard } from './pages/admin/AdminDashboard';
 import { AdminCarRentals } from './pages/admin/AdminCarRentals';
+import { AdminTours } from './pages/admin/AdminTours';
 import { AdminTransferPricing } from './pages/admin/AdminTransferPricing';
 import { AdminTransferOrders } from './pages/admin/AdminTransferOrders';
 import { AdminRentalOrders } from './pages/admin/AdminRentalOrders';
+import { AdminTourOrders } from './pages/admin/AdminTourOrders';
+import AdminDrivers from './pages/admin/AdminDrivers';
 import { ProtectedRoute } from './components/ProtectedRoute';
 import { TawkTo } from './components/TawkTo';
 import './i18n';
@@ -25,13 +31,16 @@ const libraries = ['places'];
 function AppContent() {
   return (
     <UserProvider>
-      <AdminProvider>
-        <BrowserRouter>
+      <SocketProvider>
+        <AdminProvider>
+          <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/transfers" element={<Transfers />} />
             <Route path="/car-rentals" element={<CarRentals />} />
             <Route path="/car-rentals/:carId" element={<CarRentalDetail />} />
+            <Route path="/tours" element={<Tours />} />
+            <Route path="/tours/:tourId" element={<TourDetail />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/signup" element={<SignUp />} />
@@ -42,14 +51,18 @@ function AppContent() {
               <Route index element={<AdminDashboard />} />
               <Route path="transfer-orders" element={<AdminTransferOrders />} />
               <Route path="rental-orders" element={<AdminRentalOrders />} />
+              <Route path="tour-orders" element={<AdminTourOrders />} />
+              <Route path="drivers" element={<AdminDrivers />} />
               <Route path="car-rentals" element={<AdminCarRentals />} />
+              <Route path="tours" element={<AdminTours />} />
               <Route path="transfer-pricing" element={<AdminTransferPricing />} />
             </Route>
           </Routes>
         </BrowserRouter>
-        {/* Tawk.to Live Chat Widget */}
-        <TawkTo />
-      </AdminProvider>
+          {/* Tawk.to Live Chat Widget */}
+          <TawkTo />
+        </AdminProvider>
+      </SocketProvider>
     </UserProvider>
   );
 }
